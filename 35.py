b5069d2abe16cb4c61de698b1164d626ad4bf84c
Kagesooooo <Kagesooooo@gmail.com>
2017-12-03 23:06:24 +0900

    2語にした

--- 35.py
+++ 35.py
@@ -4,6 +4,7 @@ with open('neko.txt.mecab')as f:
     pattern = re.compile(r'^(.+?)\t(.+?),(.+?),(.+?),(.+?),(.+?),(.+?),(.+?)$')
     dic = {}
     st = ''
+    cnt = 0
     for s in f:
         match = pattern.match(s)
         if match == None:
@@ -12,7 +13,9 @@ with open('neko.txt.mecab')as f:
         dic['pos'] = match.group(2)
         if dic['pos'] == '名詞':
             st += dic['surface']
+            cnt += 1
         else:
-            if st != '':
+            if st != '' and cnt > 1:
                 print(st)
+            cnt = 0
             st = ''
