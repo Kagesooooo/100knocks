ee4ebced9420d0ff5e68ceab4543f8756d8f961b
Kagesooooo <Kagesooooo@gmail.com>
2018-01-07 21:36:17 +0900

    aaaaaaaa

--- 50.py
+++ 50.py
@@ -3,4 +3,5 @@ import re
 with open('nlp.txt') as f:
     with open('output/50.txt','w') as fout:
         for l in f:
-            fout.write(re.sub(r'(.+?[\.;:\?!]) ([A-Z])',r'\1 \n\2',l))
+            if l != '\n':
+                fout.write(re.sub(r'(.+?[\.;:\?!]) ([A-Z])',r'\1 \n\2',l))
