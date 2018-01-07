bb192dc1682fd00858fc279a04da30ed0723b7c0
Kagesooooo <Kagesooooo@gmail.com>
2018-01-07 21:39:36 +0900

    doya

--- 50.py
+++ 50.py
@@ -4,4 +4,4 @@ with open('nlp.txt') as f:
     with open('output/50.txt','w') as fout:
         for l in f:
             if l != '\n':
-                fout.write(re.sub(r'(.+?[\.;:\?!]) ([A-Z])',r'\1 \n\2',l))
+                fout.write(re.sub(r'(.+?[\.;:\?!]) ([A-Z])',r'\1\n\2',l))
