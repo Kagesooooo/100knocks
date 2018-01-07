cfcbc205c3371c8acff8374f21e0faddf7a93c58
Kagesooooo <Kagesooooo@gmail.com>
2018-01-07 21:31:22 +0900

    doya

--- 50.py
+++ 50.py
@@ -3,7 +3,4 @@ import re
 with open('nlp.txt') as f:
     with open('output/50.txt','w') as fout:
         for l in f:
-            pat = re.compile(r'([A-Z].+?[\.;:\?!] )')
-            for st in pat.split(l):
-                if st[:-1] != '':
-                    fout.write(st[:-1]+'\n')
+            fout.write(re.sub(r'(.+?[\.;:\?!]) ([A-Z])',r'\1 \n\2',l))
--- aiu.py
+++ aiu.py
@@ -1,42 +1,12 @@
-import xml.etree.ElementTree as ET
 import re
 
-pattern = re.compile(r'^\((.*?) (.*?)\)$')
-
-def mk_nplist(str0, list0):
-    match = pattern.match(str0)
-    key = match.group(1)
-    value = match.group(2)
-    cnt0 = 0
-    cnt1 = 0
-    st = ''
-    words = []
-    for c in value:
-        if c == '(':
-            st += c
-            cnt0 += 1
-        elif c == ')':
-            st += c
-            cnt1 += 1
-            if cnt0 == cnt1:
-                words.append(mk_nplist(st, list0))
-                st = ''
-        # elif not (cnt0 == cnt1 and c == ' '):
-        elif cnt0 != cnt1 or c != ' ':
-            st += c
-        else:
-            continue
-    if st != '':
-        words.append(st)
-    result = ' '.join(words)
-    if key == 'NP':
-        list0.append(result)
-        print(result)
-    return result
-
-root = ET.parse('nlp.txt.xml')
-
-for parse in root.iterfind('./document/sentences/sentence/parse'):
-    result = []
-    mk_nplist(parse.text[:-1], result)
-    # print(result)
+with open('nlp.txt') as f:
+    with open('output/50.txt','w') as fout:
+        for l in f:
+            fout.write(re.sub(r'(.+?[\.;:\?!]) ([A-Z])',r'\1 \n\2',l))
+        # for l in f:
+        #     pat = re.compile(r'(.+?[\.;:\?!] [A-Z])')
+        #     for st in pat.split(l):
+        #         if st[:-1] != '':
+        #             print(st[:-1])
+        #             # fout.write(st[:-1]+'\n')
