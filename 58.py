59f3535056315c4aa0fcc062ad62b7f840fe0cc0
Kagesooooo <Kagesooooo@gmail.com>
2018-01-08 10:35:57 +0900

    uuu

--- 58.py
+++ 58.py
@@ -9,12 +9,12 @@ for sen in root.findall('document/sentences/sentence'):
     dicd = {}
     for dep in sen.findall('dependencies[@type="collapsed-dependencies"]/dep'):
         if dep.get('type') in ['nsubj','dobj']:
-            dic[dep.find('governor').get('idx')] = dep.find('governor').text
+            dic[int(dep.find('governor').get('idx'))] = dep.find('governor').text
             if dep.get('type') == 'nsubj':
-                dicn[dep.find('governor').get('idx')] = dep.find('dependent').text
+                dicn[int(dep.find('governor').get('idx'))] = dep.find('dependent').text
             else:
-                dicd[dep.find('governor').get('idx')] = dep.find('dependent').text
+                dicd[int(dep.find('governor').get('idx'))] = dep.find('dependent').text
 
     for idx in sorted(dic.keys()):
         if idx in dicn.keys() and idx in dicd.keys():
-            print(dicn[idx]+'\t'+dic[idx]+'\t'+dicd[idx])
+            print(dicn[idx]+'\t'+dic[idx]+'\t'+dicd[idx]+'\t'+str(idx))
