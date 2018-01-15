2dadddbf46501f99beae6fe3fc2b4bf96070bb27
Kagesooooo <Kagesooooo@gmail.com>
2018-01-15 12:18:21 +0900

    unko

--- 68.py
+++ 68.py
@@ -4,5 +4,7 @@ client = pymongo.MongoClient()
 db = client.testdb
 collection = db.artist
 
+cnt = 0
 for i in collection.find({'tags.value':'dance'}).sort('rating.count',-1).limit(10):
-    print(i.get('name'))
+    cnt += 1
+    print(str(cnt) + '\t' + i.get('name'))
