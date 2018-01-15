fb15a0469da8d69d19853348c61d396dc64e21ef
Kagesooooo <Kagesooooo@gmail.com>
2018-01-15 10:56:42 +0900

    unko

--- 61.py
+++ 61.py
@@ -2,4 +2,6 @@ import plyvel
 
 db = plyvel.DB('./db_test/', create_if_missing=True)
 
-print(db.get('The Silhouettes\t101060'.encode()))
+for key, value in db:
+    if 'Oasis' == (key.decode()).split('\t')[0]:
+        print(value.decode())
